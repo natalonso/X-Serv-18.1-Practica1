4e70c5adb4c524d8e5c3c1dd54bfbcf0f9bd3d45
Natalia Alonso Arteaga <nalonso@alpha29.aulas.gsyc.urjc.es>
2018-03-08 18:09:48 +0100

    Nueva version con redireccion pero sin ficheros

--- acortar_url.py
+++ acortar_url.py
@@ -17,41 +17,51 @@ class contentApp(webapp.webApp):
 
     def parse(self,request):
         #ultimo elemento del split se referencia con -1, si añado un uno quiere decir que hago el split solo una vez
-        metodo = request.split()[0]
-        recurso = request.split()[1]
-        peticion = request
-        return (metodo, recurso, peticion)
+        if len(request) > 0:
+            metodo = request.split()[0]
+            recurso = request.split()[1]
+            peticion = request
+            return (metodo, recurso, peticion)
 
+        else:
+            metodo = 0
+            recurso = 0
+            peticion = 0
+            return (metodo, recurso, peticion)
 
     def process(self, parsedRequest): #parsedRequest es una tupla(metodo,recurso, peticion)
         global contador
         metodo, recurso, peticion = parsedRequest
+        if (metodo == 0 and recurso == 0 and peticion == 0):
+            return ("200 OK", "<html>ente fantasma!!!!</html>")
+
 
         if metodo == "POST":
             cuerpo = peticion.split('\r\n\r\n', 1)[1].split('=')[1]
-            print("CUERPO: " + str(cuerpo))
-            print("LONGITUD: " + str(len(cuerpo)))
 
             if len(cuerpo) == 0: #no hay query stringo, mando mensaje de ERROR
-                return ("200 OK", "<html>DEBE RELLENAR EL CAMPO</html>")
+                return ("200 OK", "<html>DEBE RELLENAR EL CAMPO" + formulario +  "CONTENIDO DEL DICCIONARIO HASTA EL MOMENTO: " + str(dic_url) + "</html>")
+
             else: #hay qs por tanto envio un html con dos ENLACES
                 contador = contador + 1
                 dic_url[contador] = cuerpo
                 return ("200 OK", "<html><body><h1>URL ORIGINAL y URL ACORTADA: <br> URL ORIGINAL: " +
                 "<a href='https://" + str(cuerpo) + "'>"+ str(cuerpo)+ "</a><br> URL ACORTADA: " +
                 "<a href='https://" + str(cuerpo) + "'>"+ str(contador)+ "</a></h1></body></html>")
-        elif metodo == "GET":
+        else:  #metodo == "GET":
             if recurso == "/":
-                print("LONGITUD RECURSO: " + str(len(recurso)))
                 return ("200 OK", "<html>Resource not found yet!<br>" + formulario +  "CONTENIDO DEL DICCIONARIO HASTA EL MOMENTO: " + str(dic_url) + "</html>")
+            elif recurso == "/favicon.ico":
+                return ("200 OK", "<html>NOT FOUND</html>")
             else:
                 url = recurso.split("/")[1]
-                print("DICCIONARIO: " + str(dic_url))
-                redireccion = dic_url[int(url)]
-                return ("200 OK", "<html>" + str(redireccion) + "</html>")
-
-        else:
-                return ("200 OK", "<html>Recurso no encontrado</html>")
+                try:
+                    redireccion = dic_url[int(url)]
+                    return ("200 OK","<html>REDIRECCIONO<meta http-equiv="+'refresh'+ " content="+'1'+";url=" + 'https://' + str(redireccion) +"/></html>")
+                except ValueError:
+                    return ("200 OK", "<html>RECURSO NO ENCONTRADO</html>")
+                except KeyError:
+                    return ("200 OK", "<html>RECURSO NO ENCONTRADO</html>")
 
 
 if __name__ == "__main__":
