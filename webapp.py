7bdc1eb2e3096cb73050fe42a164a869d2a442d0
Natalia Alonso Arteaga <nalonso@alpha07.aulas.gsyc.urjc.es>
2018-03-12 17:58:08 +0100

    Ultima version con redirecciones y persistencia

--- webapp.py
+++ webapp.py
@@ -51,15 +51,15 @@ class webApp:
         while True:
             print('Waiting for connections')
             (recvSocket, address) = mySocket.accept()
-            print('HTTP request received (going to parse and process):')
+            print('HTTP request received (going to parse and process)')
             request = recvSocket.recv(2048).decode('utf-8')
-            print(request)
             parsedRequest = self.parse(request)
             (returnCode, htmlAnswer) = self.process(parsedRequest)
             print('Answering back...')
-            recvSocket.send(bytes("HTTP/1.1 " + returnCode + " \r\n\r\n"
-                            + htmlAnswer + "\r\n", 'utf-8))
+            recvSocket.send(bytes("HTTP/1.1 " + returnCode + " \r\n\r\n" + htmlAnswer + "\r\n", 'utf-8'))
             recvSocket.close()
+
+
 
 if __name__ == "__main__":
     testWebApp = webApp("localhost", 1234)
